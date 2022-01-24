7e12720b4da65c3bcb57a3e355c2cdf8560d079c
K8sCat <k8scat@gmail.com>
2022-01-24 13:53:14 +0800

    optimize(oschina): trace error

--- pkg/platform/oschina/client.go
+++ pkg/platform/oschina/client.go
@@ -1,8 +1,7 @@
 package oschina
 
 import (
-	"errors"
-	"fmt"
+	"github.com/juju/errors"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -26,20 +25,20 @@ func NewClient(cookie string) (*Client, error) {
 		Cookie: cookie,
 	}
 	err := parseUser(client)
-	return client, err
+	return client, errors.Trace(err)
 }
 
 // parseUser parse user data from html
 func parseUser(c *Client) error {
 	raw, err := c.Get("https://www.oschina.net/", nil, nil)
 	if err != nil {
-		return err
+		return errors.Trace(err)
 	}
 
 	// Parse base url
 	r := regexp.MustCompile(`g_user_url" data-value="([^"]+)`).FindStringSubmatch(raw)
 	if len(r) != 2 {
-		return fmt.Errorf("user url not found: %v", r)
+		return errors.Errorf("user url not found: %v", r)
 	}
 	c.BaseURL = r[1]
 
@@ -48,12 +47,12 @@ func parseUser(c *Client) error {
 	go func(c *Client) {
 		raw, err := c.Get(c.BaseURL, nil, nil)
 		if err != nil {
-			ch <- err
+			ch <- errors.Trace(err)
 			return
 		}
 		r := regexp.MustCompile(`space_user_id" data-value="(\d+)`).FindStringSubmatch(raw)
 		if len(r) != 2 {
-			ch <- fmt.Errorf("space id not found: %v", r)
+			ch <- errors.Errorf("space id not found: %v", r)
 		}
 		c.SpaceID = r[1]
 		ch <- nil
@@ -61,20 +60,20 @@ func parseUser(c *Client) error {
 	select {
 	case err := <-ch:
 		if err != nil {
-			return err
+			return errors.Trace(err)
 		}
 	}
 
 	// Parse user code
 	r = regexp.MustCompile(`g_user_code" data-value="([0-9a-zA-Z]+)`).FindStringSubmatch(raw)
 	if len(r) != 2 {
-		return fmt.Errorf("user code not found: %v", r)
+		return errors.Errorf("user code not found: %v", r)
 	}
 	c.UserCode = r[1]
 	// Parse user id
 	r = regexp.MustCompile(`g_user_id" data-value="(\d+)`).FindStringSubmatch(raw)
 	if len(r) != 2 {
-		return fmt.Errorf("user id not found: %v", r)
+		return errors.Errorf("user id not found: %v", r)
 	}
 	c.UserID = r[1]
 	return nil
@@ -84,7 +83,7 @@ func parseUser(c *Client) error {
 func (c *Client) Get(path string, params *url.Values, handler func(r *http.Response) (string, error)) (string, error) {
 	req, err := http.NewRequest(http.MethodGet, path, nil)
 	if err != nil {
-		return "", err
+		return "", errors.Trace(err)
 	}
 	req.Header.Set("Cookie", c.Cookie)
 	req.Header.Set("User-Agent", browser.Computer())
@@ -93,25 +92,26 @@ func (c *Client) Get(path string, params *url.Values, handler func(r *http.Respo
 	}
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return "", errors.Trace(err)
 	}
 
 	if handler == nil {
 		defer res.Body.Close()
 		b, err := ioutil.ReadAll(res.Body)
 		if err != nil {
-			return "", err
+			return "", errors.Trace(err)
 		}
 		return string(b), nil
 	}
 
-	return handler(res)
+	result, err := handler(res)
+	return result, errors.Trace(err)
 }
 
 func (c *Client) Post(path string, body io.Reader, handler func(r *http.Response) (string, error)) (string, error) {
 	req, err := http.NewRequest(http.MethodPost, path, body)
 	if err != nil {
-		return "", err
+		return "", errors.Trace(err)
 	}
 	req.Header.Add("Cookie", c.Cookie)
 	req.Header.Add("User-Agent", browser.Computer())
@@ -119,24 +119,26 @@ func (c *Client) Post(path string, body io.Reader, handler func(r *http.Response
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return "", err
+		return "", errors.Trace(err)
 	}
 	if handler == nil {
 		defer res.Body.Close()
 		b, err := ioutil.ReadAll(res.Body)
 		if err != nil {
-			return "", err
+			return "", errors.Trace(err)
 		}
 		return string(b), nil
 	}
-	return handler(res)
+
+	result, err := handler(res)
+	return result, errors.Trace(err)
 }
 
 func DefaultHandler(r *http.Response) (string, error) {
 	defer r.Body.Close()
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		return "", err
+		return "", errors.Trace(err)
 	}
 	raw := string(b)
 	if r.StatusCode != http.StatusOK || gjson.Get(raw, "code").Int() != 1 {
